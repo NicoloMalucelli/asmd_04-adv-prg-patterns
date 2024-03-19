c5557f366a9b3e97c4cba95003eb8347a6245c0d
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2024-03-19 15:20:20 +0100

    filter implementation

--- src/main/scala/u04/adt_verifier/Sequences.scala
+++ src/main/scala/u04/adt_verifier/Sequences.scala
@@ -46,11 +46,30 @@ object Sequences:
     type Sequence[A]
     /* constructors */
     def nil[A](): Sequence[A]
-    def cons[A](): Sequence[A]
+    def cons[A](h: A, t: Sequence[A]): Sequence[A]
     /* properties */
-    def filter[A](seq: Sequence[A], f: A => Boolean): Sequence[A]
+    def filter[A](seq: Sequence[A], f: A => Boolean): Sequence[A]/*
     def concat[A](seq: Sequence[A], seq2: Sequence[A]): Sequence[A]
     def map[A, B](seq: Sequence[A], f: A => B): Sequence[B]
     def flatMap[A, B](seq: Sequence[A], f: A => Sequence[B]): Sequence[B]
-    def fold[A](seq: Sequence[A], el: A, f: (A, A) => A): A
+    def fold[A](seq: Sequence[A], el: A, f: (A, A) => A): A*/
 
+  object SequenceDIY extends Sequence:
+    private enum SequenceImpl[A]:
+      case Nil()
+      case Cons(h: A, t: SequenceImpl[A])
+
+    import SequenceImpl.*
+
+    opaque type Sequence[A] = SequenceImpl[A]
+    override def nil[A](): Sequence[A] = Nil()
+    override def cons[A](h: A, t: Sequence[A]): Sequence[A] = Cons(h, t)
+    override def filter[A](seq: Sequence[A], f: A => Boolean): Sequence[A] = seq match
+      case Nil() => nil()
+      case Cons(h, t) => if f(h) then Cons(h, filter(t, f)) else filter(t, f)
+
+  object SequenceList extends Sequence:
+    opaque type Sequence[A] = List[A]
+    override def nil[A](): Sequence[A] = List.empty
+    override def cons[A](a:A, s:Sequence[A]): Sequence[A] = a :: s
+    override def filter[A](seq: Sequence[A], f: A => Boolean): Sequence[A] = seq.filter(f)
